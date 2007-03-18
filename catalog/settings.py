7ef8f341226dff25095dc266e6e4d5f947b49af2
denials <denials@7550b0fe-1fa9-11de-8120-8d224146dbbc>
2007-03-18 04:31:03 +0000

    Resolve isse #5 (catalog won't start due to missing settings.py).
    git-svn-id: https://kochief.googlecode.com/svn/trunk@13 7550b0fe-1fa9-11de-8120-8d224146dbbc

---
+++ catalog/settings.py
@@ -0,0 +1,44 @@
+AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',)
+DEBUG= True
+TEMPLATE_DEBUG = DEBUG
+ADMINS = ( )
+MANAGERS = ADMINS
+
+DATABASE_ENGINE = ''
+DATABASE_NAME = ''
+DATABASE_USER = ''
+DATABASE_PASSWORD = ''
+DATABASE_HOST = ''
+DATABASE_PORT = ''
+
+TIME_ZONE = "PST8PDT"
+
+CACHE_BACKEND = 'simple:///'
+
+LANGUAGE_CODE = 'en-us'
+SITE_ID = 1 
+MEDIA_URL = ''
+ADMIN_MEDIA_PREFIX = '/media/'
+
+# You should set this to something long and cryptic
+SECRET_KEY = ''
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    "django.middleware.locale.LocaleMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.middleware.doc.XViewMiddleware",
+)
+
+ROOT_URLCONF = 'urls'
+
+TEMPLATE_DIRS = (
+ )
+
+INSTALLED_APPS = ( 'catalog', )
