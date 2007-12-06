cf6520d4df2eacdb84ed0e0bd2f4d5700b6459a9
gsf747 <gsf747@7550b0fe-1fa9-11de-8120-8d224146dbbc>
2007-12-06 23:47:28 +0000

    added helios/catalog/urls.py and cleaned up helios/urls.py.  fixes #2
    git-svn-id: https://kochief.googlecode.com/svn/trunk@91 7550b0fe-1fa9-11de-8120-8d224146dbbc

--- helios/urls.py
+++ helios/urls.py
@@ -14,9 +14,6 @@
 
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns( '',
-    (r'^catalog/$', 'catalog.views.index'),
-    (r'^catalog/search/$', 'catalog.views.search'),
-    (r'^catalog/item/$', 'catalog.views.item'),
-    (r'^catalog/feed/atom/$', 'catalog.views.atomFeed'),
-    (r'^catalog/feed/rss/$', 'catalog.views.rssFeed'))
+urlpatterns = patterns('',
+    (r'^catalog/', include('helios.catalog.urls')),
+)
