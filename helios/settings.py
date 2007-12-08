558c5f99e8dac52728ab41750faa1d6d3a432835
gsf747 <gsf747@7550b0fe-1fa9-11de-8120-8d224146dbbc>
2007-12-08 01:41:46 +0000

    more small changes to helios/settings.py.  refs #3
    git-svn-id: https://kochief.googlecode.com/svn/trunk@94 7550b0fe-1fa9-11de-8120-8d224146dbbc

--- helios/settings.py
+++ helios/settings.py
@@ -37,23 +37,42 @@ DATABASE_PORT = ''             # Set to empty string for default. Not used with 
 # system time zone.
 TIME_ZONE = 'America/Chicago'
 
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
 # dummy ugettext -- see http://www.djangoproject.com/documentation/i18n/
 ugettext = lambda s: s
 
-DEFAULT_CHARSET='utf-8'
-LANGUAGE_CODE = 'en-us'
 LANGUAGES = (
     ('fr', ugettext('French')),
     ('en', ugettext('English')),
 )
 
 SITE_ID = 1 
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = ''
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
 MEDIA_URL = ''
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
 ADMIN_MEDIA_PREFIX = '/media/'
 
-# You should set this to something long and cryptic
-SECRET_KEY = ''
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'cr7)b7e+bq*u%a6f8a2d_rrcd3r_r@d3u32c-1*4^mk6fc0fsl'
 
+# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.load_template_source',
     'django.template.loaders.app_directories.load_template_source',
