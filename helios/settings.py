47829f66595c76c5f1b51a1cbb60d95e60486d7b
gsf747 <gsf747@7550b0fe-1fa9-11de-8120-8d224146dbbc>
2007-12-11 00:16:43 +0000

    in settings.py, created BASE_DIR and pointed DATABASE_NAME at a local sqlite3 file
    git-svn-id: https://kochief.googlecode.com/svn/trunk@95 7550b0fe-1fa9-11de-8120-8d224146dbbc

--- helios/settings.py
+++ helios/settings.py
@@ -14,6 +14,9 @@
 #
 # Django settings for Helios project.
 
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 DEBUG= True
 TEMPLATE_DEBUG = DEBUG
 
@@ -23,8 +26,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
+DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = BASE_DIR + '/fbo.db'             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
